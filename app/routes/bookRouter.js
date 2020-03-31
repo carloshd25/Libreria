4b2f8c9f04c5761954e1f3e7e3c995f51e14283d
carloshd25 <cartpower15@gmail.com>
2020-03-31 15:34:55 -0500

    se ajsuta validacin de autenticacion en modulo de libros

--- app/routes/bookRouter.js
+++ app/routes/bookRouter.js
@@ -1,14 +1,15 @@
 const BookController = require("../controllers/bookController");
+const authMiddleware = require('../middlewares/authMiddleware');
 
 module.exports = router => {
     router
         .route("/libro")
         .get(BookController.listBook)
-        .post(BookController.createBook);
+        .post(authMiddleware,BookController.createBook);
     router
         .route("/libro/:libroId")
         .get(BookController.findBook)
-        .delete(BookController.removeBook)
-        .put(BookController.changeBook);
+        .delete(authMiddleware,BookController.removeBook)
+        .put(authMiddleware,BookController.changeBook);
 
 };
